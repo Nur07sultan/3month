4bcd4623c20ae15e5cbde2c3be4be14c774adcd1
Nur07sultan <Aibeknurel-7@gmail.com>
2025-09-12 16:35:18 +0600

    сделал дз

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main(page: ft.Page):
     greeting_history = []
     history_text = ft.Text("История приветствий")
 
+    history_visidle = True
+
     def on_button_click(e):
         name = name_input.value.strip()
 
@@ -30,11 +32,35 @@ def main(page: ft.Page):
             greeting_text.value = "Пожалуйста, ведите имя!"
 
         page.update()
-        
+    def clear_history(_):
+        greeting_history.clear()
+        history_text.value = "История приветствий"
+        page.update()
+
+    def toggle_theme(_):
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        page.update()
+ 
+    def toggle_history (_):
+        nonlocal history_visidle
+        history_visidle = not history_visidle
+        history_text.visible = history_visidle
+        if history_visidle:
+            toggle_history_btn.text = "Скрыть историю"
+        else:
+            toggle_history_btn.text = "Показать историю"
+        page.update()
+
     name_input = ft.TextField(label='Введите имя', on_submit=on_button_click)
     name_button = ft.ElevatedButton(text='SEND', on_click=on_button_click)
+    clear_history_btn = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
+    theme_button = ft.IconButton(icon=ft.Icons.BRIGHTNESS_6, on_click=toggle_theme, tooltip='Сменить тему')
 
-    page.add(greeting_text, name_input, name_button, history_text)
+    toggle_history_btn = ft.ElevatedButton(text="Скрыть историю", on_click=toggle_history)
 
-ft.app(target=main)
+    page.add(greeting_text,name_input,name_button,clear_history_btn,theme_button,toggle_history_btn,history_text)
 
+ft.app(target=main)  
